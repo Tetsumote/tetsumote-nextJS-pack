a76b0a7ddec00860a37e0e388eb04f0606b22376
Tetsumote <unionproject88@gmail.com>
2020-03-28 14:47:20 +0100

    add basic router

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from "next/link";
+import Nav from "../components/Nav";
+import posts from "../data/posts";
+
+export default () => (
+<div>
+    <Nav/>
+    <hr/>
+    <ul>
+        {posts.map((post, index) => (
+            <li key={index}>
+                <Link href={{pathname: '/second', query: {id: index}}}>
+                    <a>{post.title}</a>
+                </Link>
+            </li>
+        ))}
+    </ul>
+</div>
+);
